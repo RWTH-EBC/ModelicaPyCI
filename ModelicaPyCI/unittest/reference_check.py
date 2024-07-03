45f662bb1d0881c1b3a66538a000337364049006
fabian.wuellhorst <fabian.wuellhorst@eonerc.rwth-aachen.de>
2024-07-03 09:43:10 +0200

    better logging

--- ModelicaPyCI/unittest/reference_check.py
+++ ModelicaPyCI/unittest/reference_check.py
@@ -188,10 +188,11 @@ class WhitelistTester(regression.Tester):
         _new_data = []
         for dat in self._data:
             if dat['model_name'] in self.whitelist_models:
-                dat['dymola']['exportFMU'] = False
                 _new_data.append(dat)
         self._data = _new_data
-
+        for dat in self._data:
+            if "ResultDirectory" not in dat:
+                logger.error("ResultDirectory not in model dat %s", dat)
 
         for iPro in range(self._nPro):
 
